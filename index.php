3bfdea54c7083c3ba49e4c2137a8e2d3cb01d4db
Unknown <elizabeth.c.c@outlook.fr>
2018-08-11 10:39:22 -0400

    avancer dans le css accueil

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 <div class="container inscription ">
         <h1>Devenez Client aujourd’hui ! </h1>
-        <p>Profitez de nos avantages Une équipe professionelle qui vous aide à prendre des décisions avisées à propos de votre argent.</p>
+        <p>Profitez de nos avantages Une équipe professionelle qui vous aide  à <br> prendre des décisions avisées à propos de votre argent.</p>
         <a class="button">Devenir Client</a>
 </div>
 
@@ -31,34 +31,21 @@
 
 
 <div class="columns services">
-    <div class="column is-offset-one-quarter">
+    <div class="column ">
         <h1>Services bancaires efficaces</h1>
         <i class="fas fa-laptop"></i>
         <p>Vous pouvez gérer vos affaires bancaires comme bon vous semble, que ce soit en ligne, en succursale et  au téléphone </p>
 
     </div>
     <div class="column ">
-        <h1>Accès à plus de 3 500 guichets automatiques.</h1>
+        <h1>Accès à plus de 3 500 guichets</h1>
         <i class="fas fa-money-check"></i>
         <p>Payer pour accéder à votre argent est injuste. Vous pouvez retirer votre argent sans frais partout au pays</p>
 
     </div>
 </div>
 
-<div class="columns ">
-    <div class="column is-offset-one-quarter blog">
-        <h1>Services bancaires efficaces</h1>
-        <i class="fas fa-laptop"></i>
-        <p>Vous pouvez gérer vos affaires bancaires comme bon vous semble, que ce soit en ligne, en succursale et  au téléphone </p>
 
-    </div>
-    <div class="column blog">
-        <h1>Accès à plus de 3 500 guichets automatiques.</h1>
-        <i class="fas fa-money-check"></i>
-        <p>Payer pour accéder à votre argent est injuste. Vous pouvez retirer votre argent sans frais partout au pays</p>
-
-    </div>
-</div>
 
 
 
